40113e19c90962ec7d02bb7bead1570df20f637d
AbsolofDarkness <nicpapa98@gmail.com>
2019-05-11 15:50:16 -0500

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MiniDrawer from './components/minidrawer';
+
+function App() {
+  return (
+    <div>
+      <MiniDrawer />
+    </div>
+  );
+}
+
+export default App;
